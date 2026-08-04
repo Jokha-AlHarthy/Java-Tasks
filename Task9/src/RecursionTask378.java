0a4893090a194eae8f16dbeb9a61e0bfded6e64a
Jokha-AlHarthy <jokha.hamed.official@gmail.com>
2026-08-04 12:23:11 +0400

    Adding Task 378 - Multiply Two Numbers (Using Addition Only)

--- Task9/src/RecursionTask378.java
+++ Task9/src/RecursionTask378.java
@@ -10,7 +10,7 @@ public class RecursionTask378 {
 
     }
 
-    //calling the function inside main
+    //calling the function inside the main
     public static void main(String[] args){
         System.out.println("multiply(3, 4)→ "+multiply(3, 4));
         System.out.println("multiply(5, 2)→ "+multiply(5, 2));
