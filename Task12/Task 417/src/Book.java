181b0f09e97e0c54047cedac22e4cf6c479ba447
Jokha-AlHarthy <jokha.hamed.official@gmail.com>
2026-08-11 15:30:54 +0400

    Adding Task 417 Many objects from one blueprint

---
+++ Task12/Task 417/src/Book.java
@@ -0,0 +1,11 @@
+public class Book {
+    //Add two attributes: String title and String author
+    String title;
+    String author;
+
+    //Add one behaviour (method): printInfo() that prints the title and the author.
+    public void printInfo(){
+        IO.println("Book title "+title+" and the author is "+author);
+    }
+
+}
---
+++ Task12/Task 417/src/Main.java
@@ -0,0 +1,17 @@
+public void main() {
+    //Inside main, build one object: Book b = new Book();
+    Book b = new Book();
+    b.author = "By Ishaq";
+    b.title = "Java Basics";
+    b.printInfo();
+
+    Book b1 = new Book();
+    b1.author = "By Fatma";
+    b1.title = "Python";
+    b1.printInfo();
+
+    Book b2 = new Book();
+    b2.author = "By Salim";
+    b2.title = "C#";
+    b2.printInfo();
+}
