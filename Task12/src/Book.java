e6cd67cf8b832b8f7ba0b28fe5b767d42202008a
Jokha-AlHarthy <jokha.hamed.official@gmail.com>
2026-08-11 14:24:43 +0400

    Update Book.java by adding Task 417 Many objects from one blueprint

--- Task12/src/Book.java
+++ Task12/src/Book.java
@@ -7,6 +7,7 @@ public class Book {
     public void printInfo(){
         IO.println("Title of book: "+title);
         IO.println("Author of the book: "+author);
+        IO.println("\n");
     }
 
     public static void main(String[] args){
@@ -14,6 +15,22 @@ public class Book {
         Book b = new Book();
         b.title = "Java Basics";
         b.author = "Maryam";
+
+        Book b1 = new Book();
+        b1.title = "Python";
+        b1.author = "by Ishaq";
+
+        Book b2 = new Book();
+        b2.title = "C++";
+        b2.author = "by Fatma";
+
+        Book b3 = new Book();
+        b3.title = "JavaScript";
+        b3.author = "by Salim";
+
         b.printInfo();
+        b1.printInfo();
+        b2.printInfo();
+        b3.printInfo();
     }
 }
