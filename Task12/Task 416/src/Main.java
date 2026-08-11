465380d78fdedddd6480b6bb21c7f7d6b92b3cba
Jokha-AlHarthy <jokha.hamed.official@gmail.com>
2026-08-11 15:16:48 +0400

    Adding Task 416 Your first class

---
+++ Task12/Task 416/src/Main.java
@@ -0,0 +1,7 @@
+public void main() {
+    //Inside main, build one object: Book b = new Book();
+    Book b = new Book();
+    b.author = "Maryam";
+    b.title = "Java Basics";
+    b.printInfo();
+}
