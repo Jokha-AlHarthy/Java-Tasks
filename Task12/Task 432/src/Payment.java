e62ead87b7bbc7d77bed5ebe5d788a4c537b637d
Jokha Al-Harthy <jokha.hamed.official@gmail.com>
2026-08-15 18:13:33 +0400

    Add Payment.java file

---
+++ Task12/Task 432/src/Payment.java
@@ -0,0 +1,6 @@
+//Make a class Payment with one method process() that prints "Processing the payment"
+public class Payment {
+    public void process(){
+        System.out.println("Processing the payment.");
+    }
+}
